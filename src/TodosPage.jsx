41af424e2b6075680f3319d74d0072240d595788
JaiDhiman <jaidhiman2000@gmail.com>
2024-09-30 18:21:35 -0700

    create action

--- src/TodosPage.jsx
+++ src/TodosPage.jsx
@@ -6,23 +6,23 @@ import { TodosIndex } from "./TodosIndex";
 export function TodosPage() {
   const [todos, setTodos] = useState([]);
 
+  const handleIndex = () => {
+    console.log("handleIndex");
+    axios.get("http://localhost:3000/todos.json").then((response) => {
+      console.log(response.data);
+      setTodos(response.data);
+    });
+  };
+
   const handleCreate = (params, successCallback) => {
     console.log("handleCreate", params);
     axios.post("http://localhost:3000/todos.json", params).then((response) => {
       setTodos([...todos, response.data]);
       successCallback();
     });
-
-    const handleIndex = () => {
-      console.log("handleIndex");
-      axios.get("http://localhost:3000/todos.json").then((response) => {
-        console.log(response.data);
-        setTodos(response.data);
-      });
-    };
-
-    useEffect(handleIndex, []);
   };
+
+  useEffect(handleIndex, []);
   return (
     <main>
       <TodosNew onCreate={handleCreate} />
